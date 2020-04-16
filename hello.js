720b78cc211b5429822ebdbfa90c9c1efa008461
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-04-16 18:20:38 +0000

    functions hello!

--- hello.js
+++ hello.js
@@ -1,9 +1,13 @@
-// const sayHello  = function () {
-//   console.log("Hello, world");
-// }
 
-// sayHello();
+/* 
+const sayHello  = function () {
+   console.log("Hello, world");
+}
 
+  sayHello();
+*/
+
+/* Example 2
 const sayHello  = function (name) {
   console.log("Hello, " + name + "!");
 }
@@ -11,3 +15,19 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+/*
+const sayHelloToConsole  = function (name) {    //console.log prints right away
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+
+const returnSayHello  = function (name) {   //return store the value in the function(doesn't print)
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+console.log(greeting);    //prints anytime in the code 
