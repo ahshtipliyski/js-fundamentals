5606c3f90830c1a70067b9c14aa2a4b8bbab1793
Anton Shtipliyski <63271234+ahshtipliyski@users.noreply.github.com>
2020-04-24 20:38:12 -0300

    Update and rename Loopy.js to ForLoop34.js

--- ForLoop34.js
+++ ForLoop34.js
@@ -1,11 +1,11 @@
 
 for (var i = 100; i <= 200; i++) {
   if ((i % 3 === 0) && (i % 4 === 0)) {
-    console.log("LoopyLighthouse");
+    console.log("AntonShtipliyski");
   } else if (i % 3 === 0) {
-    console.log("Loopy");
+    console.log("Anton");
   } else if (i % 4 === 0){
-    console.log("Lighthouse");
+    console.log("Shtipliyski");
   } else {
     console.log(i);
   }
